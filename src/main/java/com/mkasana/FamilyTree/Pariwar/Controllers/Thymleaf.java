cf7ca88ef135b03f749e18a2ebe7f776ca92d27d
uncannyChora <lkkaushik012@gmail.com>
2019-11-03 13:58:59 +0530

    Update Thymleaf.java

--- src/main/java/com/mkasana/FamilyTree/Pariwar/Controllers/Thymleaf.java
+++ src/main/java/com/mkasana/FamilyTree/Pariwar/Controllers/Thymleaf.java
@@ -3,16 +3,17 @@ package com.mkasana.FamilyTree.Pariwar.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Thymleaf {
 
         @RequestMapping(value = "/kasana")
-        public String index(Model model) {
+        public @ResponseBody Model index(Model model) {
 
             System.out.println("in /kasana Controller");
             model.addAttribute("msg",
                     "Mkasana a jar packaging example");
-            return "myView";
+            return model;
         }
 }
