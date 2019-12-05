d15b1e3273e82013a3f6aa955903e6990da2a211
mkasana <mkasana@amazon.com>
2019-12-05 19:40:02 +0530

    Session table is ready

--- src/main/resources/static/sqlScripts/Tables/CountryDetailsTable.sql
+++ src/main/resources/static/sqlScripts/Tables/CountryDetailsTable.sql
@@ -2,12 +2,12 @@ DROP TABLE IF EXISTS CountryDetails;
 
 
 CREATE TABLE CountryDetails (
-	Id INT AUTO_INCREMENT,
+	  Id INT AUTO_INCREMENT,
     CountryName VARCHAR(30) NOT NULL,
     CountryCode VARCHAR(30),
     Created LONG,
     Updated LONG,
-	Flag INT,
+	  Flag INT,
     About VARCHAR(256),
     PRIMARY KEY(Id)
 );
