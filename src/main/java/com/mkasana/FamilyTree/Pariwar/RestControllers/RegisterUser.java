e4806c930404ca75646e684c89c8f1b944eaeaed
mkasana <mkasana@amazon.com>
2019-12-07 18:34:53 +0530

    Profile picture Upload is working with new Register Controller

---
+++ src/main/java/com/mkasana/FamilyTree/Pariwar/RestControllers/RegisterUser.java
@@ -0,0 +1,63 @@
+package com.mkasana.FamilyTree.Pariwar.RestControllers;
+
+import com.mkasana.FamilyTree.Pariwar.login.LoginValidation;
+import com.mkasana.FamilyTree.Pariwar.model.LoginRequestBody;
+import com.mkasana.FamilyTree.Pariwar.model.LoginResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This Controller is for Register
+ */
+@Service
+@RestController
+public class RegisterUser {
+
+    @Autowired(required =true)
+    private LoginValidation loginValidation;
+
+    @RequestMapping(value = "/register/user", method = RequestMethod.POST, headers="Accept=application/json")
+    private void registerUser(@RequestParam("body") String body,
+                              @RequestParam("Image") MultipartFile file, @RequestHeader HttpHeaders headers) throws Exception {
+
+
+        /*
+        headers.forEach((key, value) -> {
+            System.out.printf("Parameter : %s, Value %s\n",key, value);
+        });
+
+        System.out.printf("Body Passed : %s\n",body);
+
+        System.out.printf("File Passed : %s\n",file);
+        System.out.printf("File Name : %s\n",file.getName());
+        System.out.printf("File Origional Name : %s\n",file.getOriginalFilename());
+        System.out.printf("File getContentType  : %s\n",file.getContentType());
+        System.out.printf("File isEmpty  : %s\n",file.isEmpty());
+        System.out.printf("File getSize  : %s\n",file.getSize());
+        System.out.printf("File getBytes  : %s\n",file.getBytes());
+        */
+        /*
+        File compressedImageFile = new File("1.jpeg");
+        System.out.printf("File Content  : "+ file.getInputStream());
+        file.transferTo(compressedImageFile);
+        System.out.printf("File AbsolutePath  :" + compressedImageFile.getAbsolutePath());
+        //System.out.printf("File Content  : "+ file.getInputStream());
+        //return loginValidation.validateUser(loginRequestBody);
+        */
+
+        //Image Location ../../../userProfileImages/ $(Image Name)
+        //Below Use the id ofuser to save the name if file
+        Path filepath = Paths.get("/Volumes/unix/SpringProjects/Pariwar/userProfileImages", "1.jpg");
+        file.transferTo(filepath);
+
+    }
+}
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -13,6 +13,7 @@ class LoginResponseBody {
     }
 }
 
+
 function TestsendHttpRequest()
 {
     alert("in Sed HTTP");
@@ -66,7 +67,20 @@ function showCookies()
 
 }
 
+function adjustLoginPopUpContentStyle() {
+    h = screen.height;
+    w = screen.width;
+
+    if(h-w > 200) {
+        document.getElementById("modal_content_login").style = "height:60vw; width:80vw; margin-top:"+h/4+"px;";
+    }
+
+}
+
 function checkIfAlreadyLoggedIn() {
+
+    adjustLoginPopUpContentStyle();
+
     var userName = getCookie("pariwarUsername");
     var token = getCookie("pariwarToken");
     var status = getCookie("pariwarStatus");
@@ -76,6 +90,10 @@ function checkIfAlreadyLoggedIn() {
         //location.replace("http://localhost:8081/home?username="+userName+"&token="+token+"&userId="+id);
 }
 
+
+/**
+*Call back Function for the Login validation check and it do redirect the as the the status.
+*/
 function responseFromLoginCheck(details)
 {
     var response = JSON.parse(details);
@@ -89,65 +107,225 @@ function responseFromLoginCheck(details)
     }
 }
 
-//check the login credentials
-function check_login_user()
+/**
+* functions are for the Progress bar for the Image
+*/
+function updateRegisterImageUploadProgressbar(e) {
+ var progress = document.getElementById("registerImageProgressBar");
+ progress.style.display="block";
+ var done = e.position || e.loaded, total = e.totalSize || e.total;
+ progress.value = Math.floor(done/total*1000)/10;
+}
+
+
+/**
+THIS Function is Generic for sending Http Request and calling the passed callback function,
+
+*/
+function SendHttpRequestAndReturnResponse(url, requestType, isSync, body, elementStatus, elementStatusContent, isFile, fileObject, callback)
 {
-    /* get data from login form */
-    var username=document.getElementById('login_username').value;
-    var password=document.getElementById('login_password').value;
+
     var xhttp = new XMLHttpRequest();
 
-    requestJSON = new LoginRequestBody(username,password);
+    if(isFile) {
+        var formData = new FormData();
+        formData.append("Image", fileObject);
+        xhttp.addEventListener('progress', updateRegisterImageUploadProgressbar, false);
+        if ( xhttp.upload ) {
+           xhttp.upload.onprogress = function(e) {
+                    updateRegisterImageUploadProgressbar(e);
+           };
+        }
+    }
 
     xhttp.onreadystatechange = function()
     {
   	    if(this.readyState<4)
   	    {
-     	   	document.getElementById('login_loading').innerHTML="<img src='images/loading3.gif' style='width:30%; height:100%; border-radius: 60%;'>";
+     	   	document.getElementById(elementStatus).innerHTML="<img src='images/loading3.gif' style='width:30%; height:100%; border-radius: 60%;'>";
   	    }
         else if(this.readyState==4)
             {
         	    if (this.status == 200)
     		    {
-      		        if(this.responseText==1)
+      		        if(this.responseText!=null)
                     {
-                        document.getElementById('login_loading').innerHTML="<p style='color: green;'><b>Login successful</b></p>";
-                        responseFromLoginCheck(this.responseText);
+                        document.getElementById(elementStatus).innerHTML="<p style='color: green;'><b>successful</b></p>";
+                        callback(this.responseText);
                     }
                     else
                     {
-                        document.getElementById('login_loading').innerHTML="<p style='color: red;'><b>Failed to validate</b></p>" ;
-                        responseFromLoginCheck(this.responseText);
+                        document.getElementById(elementStatus).innerHTML="<p style='color: red;'><b>Failed</b></p>" ;
+                        callback(this.responseText);
                     }
 		   	    }
      		    else
      		    {
-     		        document.getElementById('login_loading').innerHTML="<p style='color: red;'><b>Login Failed... </b></p>";
+     		        document.getElementById(elementStatus).innerHTML="<p style='color: red;'><b>Failed</b></p>";
     		    }
 		    }
     };
 
-    xhttp.open('POST', "http://localhost:8081/login/validate", true);
-    xhttp.setRequestHeader("tokenfortesting","TokenForTesting");
-    xhttp.setRequestHeader("Accept","application/json");
-    xhttp.setRequestHeader("Content-Type","application/json");
-    xhttp.send(JSON.stringify(requestJSON));
+    xhttp.open(requestType, url, isSync);
+    if(isFile) {
+        xhttp.setRequestHeader("enctype","multipart/form-data");
+        formData.append("body", body);
+        xhttp.send(formData);
+        console.log(formData);
+    } else {
+        xhttp.setRequestHeader("Accept","application/json");
+        xhttp.setRequestHeader("Content-Type","application/json");
+        xhttp.send(body);
+    }
     //location.replace("http://localhost:8081/index")
-    return this.responseText;
+    //return this.responseText;
 }
 
+
+
+
+/**
+ This Function if for Checking the User Credentials
+
+*/
+function check_login_user() {
+
+    /* get data from login form */
+    var username=document.getElementById('login_username').value;
+    var password=document.getElementById('login_password').value;
+
+    var url = "http://localhost:8081/login/validate";
+    var requestType = 'POST';
+    var elementStatus = "login_loading";
+    requestJSON = new LoginRequestBody(username,password);
+    body = JSON.stringify(requestJSON);
+
+    //Passing  responseFromLoginCheck as Callback function so as per the response this will get called and will take further action required
+    SendHttpRequestAndReturnResponse(url, requestType, false, body, elementStatus, "Kuch Bhi nhi, abhi Generic h", false, null, responseFromLoginCheck);
+
+}
 
 /**
 * This Function is to Toggle the State of the SubDiv in Registration form.
 *
 */
-function ToppleTheRegisterDiv(divName) {
+function ToppleTheRegisterDiv(divName, heading) {
    var div = document.getElementById("RegisterDevId"+divName);
    if (div.style.display === "block") {
          div.style.display = "none";
-         document.getElementById('RegisterDevButtonIcon'+divName).innerHTML='<i class="fa fa-plus" style="font-size:20px;color:white;vertical-align: middle;">      Personal Details</i>'
+         document.getElementById('RegisterDevButtonIcon'+divName).innerHTML='<i class="fa fa-plus" style="font-size:20px;color:white;vertical-align: middle;"> '+heading+'</i>';
        } else {
          div.style.display = "block";
-         document.getElementById('RegisterDevButtonIcon'+divName).innerHTML='<i class="fa fa-minus" style="font-size:20px;color:white;vertical-align: middle;">      Personal Details</i>'
+         document.getElementById('RegisterDevButtonIcon'+divName).innerHTML='<i class="fa fa-minus" style="font-size:20px;color:white;vertical-align: middle;"> '+heading+'</i>';
        }
+}
+
+/**
+* This Function is for Uploading Image
+*/
+function uploadAndShowImage(event,elementId) {
+    var image = document.getElementById(elementId);
+	image.src = URL.createObjectURL(event.target.files[0]);
+}
+
+
+/**
+* these below function are used for Registration of User.
+*/
+
+class RegisterDetails {
+    constructor() {
+        this._username = "";
+        this._password = "";
+        this._firstname = "";
+        this._lastname = "";
+        this._gender = 0;
+        this._dateofbirth = "";
+        this._profilepic = "";
+    }
+
+    set username(username) {
+        this._username = username;
+    }
+
+    set password(password) {
+        this._password = password;
+    }
+
+    set firstname(firstname) {
+        this._firstname = firstname;
+    }
+
+    set lastname(lastname) {
+        this._lastname = lastname;
+    }
+
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    set dateofbirth(dateofbirth) {
+        this._dateofbirth = dateofbirth;
+    }
+
+    set profilepic(profilepic) {
+        this._profilepic = profilepic;
+    }
+
+    get username() {
+            return this._username;
+    }
+
+    get password() {
+        return this._password;
+    }
+
+    get firstname() {
+        return this._firstname;
+    }
+
+    get lastname() {
+        return this._lastname;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    get dateofbirth() {
+        return this._dateofbirth;
+    }
+
+    get profilepic() {
+        return this._profilepic;
+        }
+
+}
+
+function responseFromRegisterRequest(response) {
+
+}
+
+function RegisterSubmitButton() {
+
+    detailObject = new RegisterDetails();
+    detailObject.userName = document.getElementById("register_username").value;
+    detailObject.password = document.getElementById("register_password").value;
+    detailObject.firstname = document.getElementById("register_firstname").value;
+    detailObject.lastname = document.getElementById("register_lastname").value;
+    detailObject.gender = document.getElementById("register_gender").value;
+    detailObject.dateofbirth = document.getElementById("register_dateOfBirth").value;
+    var file = document.getElementById("getProfleImage").files[0];
+    detailObject.profilepic = file.name;
+
+    console.log("Here Mahesh Kasana");
+    console.log(detailObject.profilepic);
+    console.log(JSON.stringify(detailObject))
+
+
+    var url = "http://localhost:8081/register/user";
+    var requestType = 'POST';
+    var elementStatus = "loading_register";
+    body = JSON.stringify(detailObject);
+    SendHttpRequestAndReturnResponse(url, requestType, false, body, elementStatus, "Kuch Bhi nhi, abhi Generic h", true, file, responseFromRegisterRequest);
 }
